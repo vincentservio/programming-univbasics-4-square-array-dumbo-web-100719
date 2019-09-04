c18f092b22eadd7c1f6b063f3aca63d69adb775c
vincent servio <vincentservio@gmail.com>
2019-09-04 22:05:35 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
+#while as the iterator 
+#take an array of nubers 
+#square each number 
+
+
+
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+
+while counter < array.length do 
+  new_array << array[counter] ** 2
+  counter+=1
+  
+end
+  p new_array
 end
